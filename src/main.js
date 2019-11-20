3ddef08d6b980258ca77a4c0577e58c977223564
carmej <carmen.mejia.pinedo@gmail.com>
2019-11-20 11:00:57 -0600

    cambios en el html

--- src/main.js
+++ src/main.js
@@ -1,15 +1,9 @@
-/**
- * import POKEMON from './data/pokemon/pokemon.js'
- * import LoL from './data/lol/lol.js'
- * import POTTER from './data/potter/potter.js'
- */
+import POKEMON from './data/pokemon/pokemon.js';
 
-import { example } from './data.js';
+const dataPokemon=POKEMON.pokemon;
+let result = dataPokemon.filter (function(los) {
+return (los.name === "Caterpie");
+});
+console.log(result);
 
-console.log(example);
 
-/*
- * console.log(POKEMON);
- * console.log(LoL);
- * console.log(POTTER)
-*/
