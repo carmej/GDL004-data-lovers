6372eb1af4c75368cbcc900dd151631254ff2690
carmej <carmen.mejia.pinedo@gmail.com>
2019-12-05 10:54:49 -0600

    iniciando LoL

--- src/data.js
+++ src/data.js
@@ -3,5 +3,3 @@ import pokemon from "./data/pokemon/pokemon";
 export const POKEMON = (POKEMON) => {
   return 'POKEMON';
 };
-export const ordenZZAA = dataPokemon.sort((personaje1, personaje2) =>{
-  return (personaje1.name > personaje2.name) ? - 1 : 1});
